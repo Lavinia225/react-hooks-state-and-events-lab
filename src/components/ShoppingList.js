1be7c1be7e47e531d274698e84c3adc1592a36fb
Luna Seifert <thepuddingdeity@gmail.com>
2022-11-23 14:41:01 -0700

    commit tree c92e1990a593ce63325912cd4a7912b42d5f8713

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState('All')
+
+  function setCategory(e){
+    setSelectedCategory(e.target.value)
+    debugger
+  }
+
+  function categoryChecker(item){
+    if (selectedCategory === 'All'){
+      return <Item key={item.id} name={item.name} category={item.category}/>
+    }
+    else if(item.category === selectedCategory){
+      return <Item key={item.id} name={item.name} category={item.category}/>
+    }
+  }
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={setCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +29,10 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {items.map(item => categoryChecker(item))}
       </ul>
     </div>
   );
-}
+  }
 
 export default ShoppingList;
